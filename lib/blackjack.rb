1fc10386ace999a3b101dfa6c472cf2f21901aa1
N. Uzoma Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-24 20:53:37 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,7 +53,7 @@ end
 def runner
   welcome
   card_total = initial_round
-  unitl card_total > 21 do
+  until card_total > 21 
     card_total = hit?(card_total)
     display_card_total(card_total)
   end
